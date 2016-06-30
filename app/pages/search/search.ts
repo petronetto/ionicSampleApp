661c561463b37ea1e115facca0e9ea57965c2ab3
Juliano Petronetto <juliano@petronetto.com.br>
2016-06-30 17:20:20 -0300

    Filters created

--- app/pages/search/search.ts
+++ app/pages/search/search.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ActionSheet } from 'ionic-angular';
 import { Http } from '@angular/http';
 import { Itunes } from '../../providers/itunes/itunes';
 
@@ -10,23 +10,71 @@ import { Itunes } from '../../providers/itunes/itunes';
 export class SearchPage {
   results: Array<any>;
   keyword: string;
+  usesFilter: boolean;
+  _unfilteredResults: Array<any>;
 
   constructor(private nav: NavController, private itunes: Itunes) {
     this.nav = nav;
-    this.itunes = itunes;
     this.results = [];
+    this._unfilteredResults = [];
+    this.usesFilter = false;
     this.keyword = '';
+    this.itunes = itunes;
   }
 
   keyHasBeenPressed(e) {
     if (e.keyIdentifier === 'Enter') {
-      this.itunes.search(this.keyword)
-        .then((results) => this.results = results);
+      this.itunes.search(this.keyword).then((results) => {
+        this.results = results;
+        this._unfilteredResults = results;
+        this.usesFilter = false;
+      });
     }
   }
 
   userPressedCancel() {
     this.keyword = '';
+  }
+
+  openFilters() {
+    let sheet = ActionSheet.create({
+      title: 'Filtred by...',
+      buttons: [
+        {
+          text: 'Movies only',
+          handler: () => {
+            this.results = this._unfilteredResults.filter(
+              (item) => item.kind === 'feature-movie'
+            );
+            this.usesFilter = true;
+            console.log(this._unfilteredResults);
+          }
+        },
+        {
+          text: 'Songs only',
+          handler: () => {
+            this.results = this._unfilteredResults.filter(
+              (item) => item.kind === 'song'
+            );
+            this.usesFilter = true;
+          }
+        },
+        {
+          text: 'Clear',
+          style: 'destructive',
+          handler: () => {
+            this.results = this._unfilteredResults;
+            this.usesFilter = false;
+          }
+        },
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        }
+      ]
+    })
+    this.nav.present(sheet);
+    //console.log(this._unfilteredResults);
   }
 
 }
